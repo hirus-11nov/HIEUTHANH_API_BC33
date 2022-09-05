595a039448dfd53830ef180a5ddc434ee17d4823
hieu.tb <hirusmeo@gmail.com>
2022-09-06 00:55:04 +0700

    Update register data formate

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -3,8 +3,7 @@ document.querySelector("#btnDangKy").onclick = function () {
     member.email = document.querySelector("#emailMember").value;
     member.password = document.querySelector("#passwordMember").value;
     member.name = document.querySelector("#nameMember").value;
-    //   member.passwordConfirm = document.querySelector("#passwordMember").value;
-    member.gender = document.querySelector("input[name=gender]:checked").value;
+    member.gender = JSON.parse(document.querySelector("input[name=gender]:checked").value);
     member.phone = document.querySelector("#phoneMember").value;
     var promise = axios({
         url: "https://shop.cyberlearn.vn/api/Users/signup",
