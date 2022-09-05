68d18c40e6f0547833c07c5f291f41e5990fc331
thanhnt <nguyentruongthanh0612@gmail.com>
2022-09-05 15:47:27 +0700

    add API Register

---
+++ controllers/register.js
@@ -0,0 +1,24 @@
+document.querySelector("#btnDangKy").onclick = function () {
+  var member = new Member();
+  member.email = document.querySelector("#emailMember").value;
+  member.password = document.querySelector("#passwordMember").value;
+  member.name = document.querySelector("#nameMember").value;
+  //   member.passwordConfirm = document.querySelector("#passwordMember").value;
+  member.gender =
+    document.querySelector("input[name=gender]:checked").value === "true";
+  member.phone = document.querySelector("#phoneMember").value;
+  //   console.log(typeof member.gender);
+  console.log("member", member);
+  var promise = axios({
+    url: "https://shop.cyberlearn.vn/api/Users/signup",
+    method: "POST",
+    // responseType: JSON,
+    data: member,
+  });
+  promise.then(function (result) {
+    console.log("result", result.data);
+  });
+  promise.catch(function (err) {
+    console.log(err);
+  });
+};
